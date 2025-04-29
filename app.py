d54639bc5431d0ad06a036bb1d4c65aa1a6b2ef7
ichigatsu <firstmoon1986@gmail.com>
2025-04-29 20:52:47 +0900

    flaskへの移行処理中

--- app.py
+++ app.py
@@ -1,17 +1,96 @@
-from flask import Flask, render_template
-from test import import_test, list_kari
+from flask import Flask, render_template, request, redirect, url_for
+from utils import Game
 
 app = Flask("shiritori")
+game = Game() # utils.pyのGame()をインスタンス化
 
-@app.route("/")
-def test():
+#! 動作確認用
+# game.selected_word_cpu = "しりとり"
+# game.worddict_used.append("しりとり")
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    msg = "" #cpuに表示させるメッセージ
+    if request.method == 'POST':
+        turn = request.form.get('turn')
+        if turn == "先攻":
+            game.is_user_first = True
+            msg = "お好きな言葉を入力してください！"
+        elif turn == "後攻":
+            game.is_user_first = False
+            msg = "CPUの番です"
+            print(game.is_user_first)
+        else:
+            print("おかしなことが起きている")
+    print(game.is_user_first)
     return render_template(
         'index.html',
-        message = 'shiritori apuri !!',
-        msg = import_test(),
-        list_kari = list_kari
-        )
+        game = game,
+        msg = msg
+    )
 
+
+@app.route("/game", methods=['GET', 'POST'])
+def main():
+    msg = ""
+    game.alert_message = ""
+
+    if request.method == 'POST':
+        print("POSTだよ")
+        # 既にゲームが終了している場合、入力を受け付けない
+        if game.flag_gameend == True:
+            msg = "もう一度遊ぶ場合は「再スタート」を押してください"
+            return render_template(
+                'index.html',
+                game = game,
+                msg = msg,
+                lists = game.worddict_used
+            )
+        # userが後攻を選んだ時の対応
+        if game.is_user_first == False:
+            print("今/gameの中でis_user_firstの確認中")
+            game.inputed_word_user = ""
+            print("game.is_user_first == False")
+            game.selected_word_cpu, game.worddict_cpu, game.worddict_used = game.choice_word_cpu(inputed, game.worddict_cpu, game.worddict_used)
+            return render_template(
+                'index.html',
+                game = game,
+                msg = game.selected_word_cpu,
+                lists = game.worddict_used
+            )
+        else:
+            # ゲームを進行する
+            inputed = request.form['inputed']
+            game.checkInputedUser(inputed, game.worddict_used, game.selected_word_cpu)
+            #userの入力文字に不備があった場合：
+            if game.alert_message:
+                msg = game.alert_message
+            else:
+                game.selected_word_cpu, game.worddict_cpu, game.worddict_used = game.choice_word_cpu(inputed, game.worddict_cpu, game.worddict_used)
+                if not game.selected_word_cpu:
+                    msg = game.alert_message
+                else:
+                    msg = game.selected_word_cpu
+            return render_template(
+                'index.html',
+                game = game,
+                msg = msg,
+                lists = game.worddict_used
+            )
+    else:
+        return render_template(
+            'index.html',
+            game = game,
+            msg = msg,
+            lists = game.worddict_used
+        )
+    
+@app.route("/reset", methods=['POST'])
+def reset():
+        # 全ての項目をリセットする
+        game.allReset()
+        return redirect(url_for("index"))   # ← PRGパターン
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,22 +1,29 @@
-# 使用する変数の整理…グローバルとローカルで使う名前を変える
-
-import unicodedata, json, random
+# import文は一つずつ書くのがPythonのルールのようだ
+import unicodedata
+import json
+import random
 
 class Game:
     def __init__(self):
-        # この辺app.pyで設定しないとダメな気もする、大丈夫な気もする
-        self.worddict_cpu = {} # CPUが使う辞書の読込
+        self.worddict_cpu = self.fileOpen() # CPUが使う辞書の読込
         self.worddict_used = [] # user, cpuが使った単語を格納するためのリスト
         self.selected_word_cpu = "" # cpuが前回選んだ単語
         self.inputed_word_user = "" # userが前回入力した単語
         self.next_letter = "" # 次の単語の頭文字
+        self.alert_message = "" # エラー時のメッセージ
+        self.n_count = 0 # 「ん」で終わろうとするユーザチェック用
+        self.flag_gameend = False # ゲームが修了した後の処理用フラグ
+        self.is_user_first = None
 
-    def fileOpen(self): # cpuの辞書を取得する
-        with open('worddict.json', mode="r", encoding="utf-8") as f:
-            # worddict_cpu = json.load(f)
-            return json.load(f)
+    def fileOpen(self): # cpuが使う辞書を取得する
+        try:
+            with open('worddict.json', mode="r", encoding="utf-8") as f:
+                return json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            print(f'絶対に起きないはずのエラー、起きたらファイルが消えてる: {e}')
+            return {}
 
-    # 前回の文字の終わりと今回の文字の頭の比較用の関数。cpuの場合、
+    # 前回の文字の終わりと今回の文字の頭の比較用の関数。
     def lastletterChecker(self, prev_word, follow_word):
         prev_letter = prev_word[-1]
         # 最後の文字が伸ばし棒の場合、一つ前の文字をとる
@@ -33,114 +40,93 @@ class Game:
         follow_letter = follow_word[0] # 次のターンがuserの場合、follow_letterから始まる単語を使わせる
         return prev_letter, follow_letter
 
-    #! userの入力内容をチェックする。
-    #! 元々Python単体で完結できるようにcontinueを使ったがbreakでerror_messageを返すべきかも
     #* 引数について：inputed=userがブラウザでinputした単語, used=worddict_used, cpu=selected_word_cpu
-    def check_inputed_user(self, inputed, used, cpu):
-        n_count = 0 # 「ん」で終わらせようとするuser向けチェッカー
+    def checkInputedUser(self, inputed, used, cpu):
         # ユーザの入力チェック
         while True:
-        #* 未入力チェック...これはブラウザ側でやるから必要ない可能性が高い
-            # inputed = input('ひらがなを入力してください：')
-            if inputed == "":
-                print("空白のままでは送信できません")
-                continue
-
             # 入力された単語はひらがなか？
             all_hiragana = True
             for letter in inputed:
-                # print(unicodedata.name(letter))
                 if 'HIRAGANA' not in unicodedata.name(letter):
                     all_hiragana = False
 
             if not all_hiragana:
                 print('注意：入力は平仮名のみにしてください！')
-                continue
+                self.alert_message = '注意：入力は平仮名のみにしてください！'
+                return self.alert_message
 
             # 入力された単語は使用済みか？
             if inputed in used:
                 print('注意：その単語は既に使われています！')
-                continue
+                self.alert_message = '注意：その単語は既に使われています！'
+                return self.alert_message
 
             # cpuの単語から続く言葉を入力しているか？
             if cpu != "": #userが先行の場合はスキップ
                 prev, follow = self.lastletterChecker(cpu, inputed)
                 if prev != follow:
-                    print("CPUの言葉に続く単語を入力してください！")
-                    continue
+                    self.alert_message = 'CPUの言葉に続けてください（濁音・半濁音不可）！'
+                    return self.alert_message
 
             # 「ん」で終わる単語を入れるuserがいたら必死に止める
             if inputed[-1] == "ん":
-                if n_count == 0:
-                    print("本当に「ん」で終わる単語を入れますか？")
-                    n_count += 1
-                    continue
-                elif n_count == 1:
-                    print("後悔しませんね？")
-                    n_count += 1
-                    continue
-                elif n_count == 2:
-                    print("本当に後悔しませんね？")
-                    n_count += 1
-                    continue
+                if self.n_count == 0:
+                    self.n_count += 1
+                    self.alert_message = "本当に「ん」で終わる単語を入れますか？"
+                    return self.n_count, self.alert_message
+
+                elif self.n_count == 1:
+                    self.n_count += 1
+                    self.alert_message = "後悔しませんね？"
+                    return self.n_count, self.alert_message
+
                 else:
-                    print("CPUの勝ちですって言うかCPUに勝たせました")
-                    inputed = None
-                    return inputed
+                    self.alert_message = "あなたは負けました…"
+                    self.flag_gameend = True
+                    return inputed, used
 
-            print(f'入力文字：{inputed}')
-
+            # 使用済み辞書にuserが入力した単語を追加
             used.append(inputed)
-            print(f'returnされたinputed: {inputed}')
-            print(f'これまでに使われた単語：{used}')
-
             return inputed, used
 
 
     def choice_word_cpu(self, inputed, dict_cpu, dict_used): # CPUが単語を選ぶ時の処理
+        print("CPUが言葉を選んでいます")
         if inputed == "": # cpu先攻の場合は辞書からランダムに選ぶ
             letter = random.choice(list(dict_cpu.keys()))
         else:
-            letter = self.lastletterChecker(inputed, inputed)
-            if len(dict_cpu[letter]) == 0:
-                print("CPUが出せる単語はもうありません。あなたの勝ちです")
-                return None
+            letter, _ = self.lastletterChecker(inputed, inputed)
+            # cpuの返す言葉がなくなっている場合：
+            if letter not in dict_cpu:
+                self.alert_message = "CPUが出せる単語はもうありません。あなたの勝ちです！"
+                selected = None
+                self.flag_gameend = True
+                return selected, dict_cpu, dict_used
 
-        selected = dict_cpu[letter][0]
-        print(f'CPUが選んだ単語： {selected}')
+        # 辞書の中からランダムで言葉を選ぶ
+        while True:
+            selected = random.choice(dict_cpu[letter])
+            if selected not in dict_used:
+                break
 
         # 使用済み辞書への追加と、cpuが使う辞書からの使用した単語の除去
         dict_used.append(selected)
-        dict_cpu[selected[0]].remove(selected)
+        # 辞書の残りが1の場合、辞書から該当インデックスごと削除する
+        if len(dict_cpu[selected[0]]) == 1:
+            del dict_cpu[selected[0]]
+        else:
+            dict_cpu[selected[0]].remove(selected)
 
-        print(f'returnされたselected: {selected}')
-        print(f'これまでに使われた単語：{dict_used}')
         return selected, dict_cpu, dict_used
 
-#! 以下テストプレイ用のコード
-gameend_flag = False
-while gameend_flag == False:
-    game_start = input('先攻でプレイしますか？ y/n: ')
-    if game_start.lower() == 'y':
-        while True:
-            inputed_word_user = check_inputed_user() #! 要調整：ブラウザで入力した内容を反映させる
-            if inputed_word_user == None:
-                gameend_flag = True
-                break
-            selected_word_cpu = choice_word_cpu(inputed_word_user)
-            if selected_word_cpu == None:
-                gameend_flag = True
-                break
-    elif game_start.lower() == 'n':
-        while True:
-            selected_word_cpu = choice_word_cpu(inputed_word_user)
-            if selected_word_cpu == None:
-                gameend_flag = True
-                break
-            inputed_word_user = check_inputed_user()
-            if inputed_word_user == None:
-                gameend_flag = True
-                break
-    else:
-        print('y または n を入力してください')
-        continue
+    # *最初からやり直す用関数
+    def allReset(self):
+        self.worddict_cpu = self.fileOpen() # CPUが使う辞書の読込
+        self.worddict_used = [] # user, cpuが使った単語を格納するためのリスト
+        self.selected_word_cpu = "" # cpuが前回選んだ単語
+        self.inputed_word_user = "" # userが前回入力した単語
+        self.next_letter = "" # 次の単語の頭文字
+        self.alert_message = "" # エラー時のメッセージ
+        self.n_count = 0 # 「ん」で終わろうとするユーザチェック用
+        self.flag_gameend = False
+        self.is_user_first = None
