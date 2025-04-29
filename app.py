793c2f957e83cbc6be5b40b45b0f0614031aa8ba
ichigatsu <firstmoon1986@gmail.com>
2025-04-30 07:12:55 +0900

    css再調整, requirements.txt追加

--- app.py
+++ app.py
@@ -93,5 +93,6 @@ def reset():
 
 if __name__ == '__main__':
     # app.debug = True
+    # app.run(host='127.0.0.1') # ローカル環境用
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1')
+    app.run(host='0.0.0.0', port=port)
